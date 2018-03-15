11b046dbe6f4928663f90f641be857a919960d48
y.bing <y.bing@msn.com>
2018-03-15 22:40:24 +0800

    add configuration

--- www/coroweb.py
+++ www/coroweb.py
@@ -74,3 +74,88 @@ def has_request_arg(fn):
             raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
     return found
 
+class RequestHandler(object):
+
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_name_kw_args = has_name_kw_args(fn)
+        self._name_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
+    
+    async def __call__(self, request):
+        kw = None
+        if self._has_var_kw_arg or self._has_name_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest(text='Missing Content-Type.')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest(text='JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencode') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest(text='Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._name_kw_args:
+                #remove all unamed kw:
+                copy = dict()
+                for name in self._name_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            #check named arg:
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        
+        if self._has_request_arg:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest(text='Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+
+
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static', path))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == -1:
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n + 1]
+        mod = getattr(__import__(module_name[:n], globals(), locals(),[name]),name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn,'__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_routes(app, fn)
+    
+
